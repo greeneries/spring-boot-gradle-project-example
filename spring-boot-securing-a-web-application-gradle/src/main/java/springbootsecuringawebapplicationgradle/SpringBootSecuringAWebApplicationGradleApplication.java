0e9762bbb6ee802bf053af80cd6ae94f8e90cd3b
greenery <elindreams@hanmail.net>
2018-10-19 17:45:21 +0900

    .

---
+++ spring-boot-securing-a-web-application-gradle/src/main/java/springbootsecuringawebapplicationgradle/SpringBootSecuringAWebApplicationGradleApplication.java
@@ -0,0 +1,12 @@
+package springbootsecuringawebapplicationgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuringAWebApplicationGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecuringAWebApplicationGradleApplication.class, args);
+	}
+}
